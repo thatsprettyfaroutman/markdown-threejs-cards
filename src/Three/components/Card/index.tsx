43e89e24be9327fc221d7072c7e0b8b9ac528c9e
thatsprettyfaroutman <viljami.anttonen@gmail.com>
2022-11-20 13:50:17 +0700

    feat: gltf card

--- src/Three/components/Card/index.tsx
+++ src/Three/components/Card/index.tsx
@@ -1,14 +1,13 @@
-import { useRef } from 'react'
 import { NearestFilter } from 'three'
-import { useFrame } from '@react-three/fiber'
-
-import { useTexture } from '@react-three/drei'
+import { useTexture, useGLTF } from '@react-three/drei'
 
 const textureConfig = (texture) => {
   texture.minFilter = NearestFilter
 }
 
-export const Card = ({ ...restProps }) => {
+export const Card = (props) => {
+  const { nodes, materials } = useGLTF('/models/card.gltf')
+
   const [texture, specularColor, metal, roughness] = useTexture(
     [
       '/textures/diffuseStroke.png',
@@ -28,58 +27,40 @@ export const Card = ({ ...restProps }) => {
     textureConfig
   )
 
-  // This reference will give us direct access to the mesh
-  const mesh = useRef()
-  // Set up state for the hovered and active state
-  // const [hovered, setHover] = useState(false)
-  // const [active, setActive] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => {
-    if (!mesh.current) {
-      return
-    }
-
-    // @ts-ignore
-    // mesh.current.rotation.x += 0.01
-  })
-  // Return view, these are regular three.js elements expressed in JSX
   return (
-    <mesh
-      {...restProps}
-      ref={mesh}
-      receiveShadow
-      castShadow
-      // onPointerMove={(e) => console.log(e.pointer)}
-      // scale={active ? 1.5 : 1}
-      // onClick={(event) => setActive(!active)}
-      // onPointerOver={(event) => setHover(true)}
-      // onPointerOut={(event) => setHover(false)}
-    >
-      <boxGeometry args={[1, 1.4, 0.005]} />
-
-      <meshPhysicalMaterial attach="material-0" color="#0D0E1A" specular={1} />
-      <meshPhysicalMaterial attach="material-1" color="#0D0E1A" specular={1} />
-      <meshPhysicalMaterial attach="material-2" color="#0D0E1A" specular={1} />
-      <meshPhysicalMaterial attach="material-3" color="#0D0E1A" specular={1} />
-      <meshPhysicalMaterial attach="material-4" color="#0D0E1A" specular={1} />
-      <meshPhysicalMaterial
-        attach="material-4"
-        map={texture}
-        roughness={0.5}
-        roughnessMap={roughness}
-        metalnessMap={metal}
-        specularColorMap={specularColor}
-        transparent
-      />
-      <meshPhysicalMaterial
-        attach="material-5"
-        map={backTexture}
-        roughness={0.5}
-        roughnessMap={backRoughness}
-        metalnessMap={backMetal}
-        specularColorMap={backSpecularColor}
-        transparent
-      />
-    </mesh>
+    <group {...props} dispose={null}>
+      <group rotation-y={Math.PI * -0.5} rotation-z={Math.PI} scale-x={-1}>
+        <mesh castShadow receiveShadow geometry={nodes.Cube002.geometry}>
+          <meshPhysicalMaterial
+            attach="material"
+            map={texture}
+            roughness={0.5}
+            roughnessMap={roughness}
+            metalnessMap={metal}
+            specularColorMap={specularColor}
+            transparent
+          />
+        </mesh>
+        <mesh castShadow receiveShadow geometry={nodes.Cube002_1.geometry}>
+          <meshPhysicalMaterial
+            attach="material"
+            map={backTexture}
+            roughness={0.5}
+            roughnessMap={backRoughness}
+            metalnessMap={backMetal}
+            specularColorMap={backSpecularColor}
+            transparent
+          />
+        </mesh>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube002_2.geometry}
+          material={materials.Edge}
+        />
+      </group>
+    </group>
   )
 }
+
+useGLTF.preload('/models/card.gltf')
